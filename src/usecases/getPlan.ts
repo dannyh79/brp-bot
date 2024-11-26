156982403d29d1fe5ba58eeed19f74d4be55f1b5
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-11-26 18:20:19 +0800

    Fix type inference in schema

--- src/usecases/getPlan.ts
+++ src/usecases/getPlan.ts
@@ -1,10 +1,10 @@
+import { Str } from 'chanfana';
 import { z } from 'zod';
-import { Arr, Str } from 'chanfana';
 
 export const Plan = z.object({
   date: Str({ example: '2024-11-26' }),
   scope: Str({ example: '創世紀 23' }),
-  content: Arr(
+  content: z.array(
     Str({ example: '1. 從亞伯拉罕與赫人和以弗倫的對話中你可以看見他是一位怎麼樣的人呢?為什麼?' }),
   ),
 });
