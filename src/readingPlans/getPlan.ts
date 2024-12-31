05e6475d1a23e10cfca3052ed72ca233a1a26ed7
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-12-31 14:44:24 +0800

    Implement usecase getPlan for 2025 reading plans

--- src/readingPlans/getPlan.ts
+++ src/readingPlans/getPlan.ts
@@ -1,5 +1,5 @@
 import { z } from '@hono/zod-openapi';
-import { OldPlan } from './entities/plan';
+import { OldPlan, Plan } from './entities/plan';
 
 export type GetPlanArgs = {
   date: string;
@@ -28,3 +28,39 @@ export const get2024Plan: UsecaseConstructor<Repository<OldPlan>, GetPlanArgs, G
   };
 
 export default get2024Plan;
+
+export const GetPlanOutputSchema = z.object({
+  date: z.string().openapi({ example: '2025-01-01' }),
+  praise: z.object({
+    scope: z.string().openapi({ example: '歷代志上 16:34 CCB' }),
+    content: z.string().openapi({ example: '你們要稱謝耶和華, 因為祂是美善的, 祂的慈愛永遠長存!' }),
+  }),
+  repentence: z.string().openapi({
+    example:
+      '上帝啊，求你按你的慈愛恩待我！\n按你極大的憐憫除去我 ___ 的過犯！\n求你洗淨我的罪過，清除我的罪惡。求你讓我重新享受蒙你拯救的喜樂，賜我一個樂意順服你的心靈。',
+  }),
+  devotional: z.object({
+    scope: z.string().openapi({ example: '出埃及記 第八章' }),
+    content: z.array(z.string()).openapi({
+      example: ['1. 你覺得神透過今天的經文對你說什麼呢?', '2. 有什麼你可以做出的行動或改變呢?'],
+    }),
+  }),
+  prayer: z.string().openapi({
+    example:
+      '”神啊！我將我的，___ ， ___ ， ___ 交給祢，我相信祢在這些事上掌權。\n『我們在天上的父：願人都尊你的名為聖。願你的國降臨；願你的旨意行在地上，如同行在天上。我們日用的飲食，今日賜給我們。免我們的債，如同我們免了人的債。不叫我們陷入試探；救我們脫離那惡者。因為國度、權柄、榮耀，全是你的，直到永遠。阿們！』”',
+  }),
+});
+
+export type GetPlanOutput = z.infer<typeof GetPlanOutputSchema>;
+
+export const getPlan: UsecaseConstructor<Repository<Plan>, GetPlanArgs, GetPlanOutput> =
+  (repo) => async (args) => {
+    const { date } = args;
+    const plan = await repo.findById(date);
+
+    if (!plan) {
+      return null;
+    }
+
+    return plan;
+  };
--- test/usecases/getPlan.spec.ts
+++ test/usecases/getPlan.spec.ts
@@ -1,4 +1,4 @@
-import { OldPlan, get2024Plan } from '@/readingPlans';
+import { OldPlan, Plan, PlanSchema, get2024Plan, getPlan } from '@/readingPlans';
 
 const stub2024Plan = {
   date: '2024-11-26',
@@ -6,14 +6,14 @@ const stub2024Plan = {
   content: ['1. 從亞伯拉罕與赫人和以弗倫的對話中你可以看見他是一位怎麼樣的人呢?為什麼?'],
 };
 
-class StubRepository implements Repository<OldPlan> {
+class StubOldPlanRepository implements Repository<OldPlan> {
   findById(date: string): Promise<OldPlan | null> {
     return Promise.resolve(date === stub2024Plan.date ? stub2024Plan : null);
   }
 }
 
 describe('get2024Plan()', () => {
-  const repo = new StubRepository();
+  const repo = new StubOldPlanRepository();
 
   it('returns plan object', async () => {
     expect(await get2024Plan(repo)({ date: '2024-11-26' })).toMatchObject(stub2024Plan);
@@ -21,5 +21,36 @@ describe('get2024Plan()', () => {
 
   it('returns null', async () => {
     expect(await get2024Plan(repo)({ date: '2024-11-32' })).toBeNull();
+  });
+});
+
+const stubData = {
+  date: '2025-01-01',
+  praise: {
+    scope: '歷代志上 16:34 CCB',
+    content: '你們要稱謝耶和華, 因為祂是美善的, 祂的慈愛永遠長存!',
+  },
+  devotional: {
+    scope: '出埃及記 第八章',
+  },
+};
+
+const parsedStubData = PlanSchema.parse(stubData);
+
+class StubRepository implements Repository<Plan> {
+  findById(date: string): Promise<Plan | null> {
+    return Promise.resolve(date === stubData.date ? parsedStubData : null);
+  }
+}
+
+describe('getPlan()', () => {
+  const repo = new StubRepository();
+
+  it('returns plan object', async () => {
+    expect(await getPlan(repo)({ date: '2025-01-01' })).toMatchObject(parsedStubData);
+  });
+
+  it('returns null', async () => {
+    expect(await getPlan(repo)({ date: '2024-12-31' })).toBeNull();
   });
 });
