af1b8e23e94a310279977ffe734023d912437a1c
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-12-31 15:55:26 +0800

    Remove usecase get2024Plan()

--- src/readingPlans/getPlan.ts
+++ src/readingPlans/getPlan.ts
@@ -1,31 +1,9 @@
 import { z } from '@hono/zod-openapi';
-import { OldPlan, Plan } from './entities/plan';
+import { Plan } from './entities/plan';
 
 export type GetPlanArgs = {
   date: string;
 };
-
-export const Get2024PlanOutputSchema = z.object({
-  date: z.string().openapi({ example: '2024-11-26' }),
-  scope: z.string().openapi({ example: '創世紀 23' }),
-  content: z.array(z.string()).openapi({
-    example: ['1. 從亞伯拉罕與赫人和以弗倫的對話中你可以看見他是一位怎麼樣的人呢?為什麼?'],
-  }),
-});
-
-export type Get2024PlanOutput = z.infer<typeof Get2024PlanOutputSchema>;
-
-export const get2024Plan: UsecaseConstructor<Repository<OldPlan>, GetPlanArgs, Get2024PlanOutput> =
-  (repo) => async (args) => {
-    const { date } = args;
-    const plan = await repo.findById(date);
-
-    if (!plan) {
-      return null;
-    }
-
-    return plan;
-  };
 
 export const GetPlanOutputSchema = z.object({
   date: z.string().openapi({ example: '2025-01-01' }),
--- test/usecases/getPlan.spec.ts
+++ test/usecases/getPlan.spec.ts
@@ -1,28 +1,4 @@
-import { OldPlan, Plan, PlanSchema, get2024Plan, getPlan } from '@/readingPlans';
-
-const stub2024Plan = {
-  date: '2024-11-26',
-  scope: '創世紀 23',
-  content: ['1. 從亞伯拉罕與赫人和以弗倫的對話中你可以看見他是一位怎麼樣的人呢?為什麼?'],
-};
-
-class StubOldPlanRepository implements Repository<OldPlan> {
-  findById(date: string): Promise<OldPlan | null> {
-    return Promise.resolve(date === stub2024Plan.date ? stub2024Plan : null);
-  }
-}
-
-describe('get2024Plan()', () => {
-  const repo = new StubOldPlanRepository();
-
-  it('returns plan object', async () => {
-    expect(await get2024Plan(repo)({ date: '2024-11-26' })).toMatchObject(stub2024Plan);
-  });
-
-  it('returns null', async () => {
-    expect(await get2024Plan(repo)({ date: '2024-11-32' })).toBeNull();
-  });
-});
+import { Plan, PlanSchema, getPlan } from '@/readingPlans';
 
 const stubData = {
   date: '2025-01-01',
