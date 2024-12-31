64797e8f82c505cc9c50bab2f546937be5e25ac3
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-12-31 15:57:10 +0800

    Remove for-2024-reading-plan InMemory2024PlanRepository

--- test/repositories/inMemoryPlanRepository.spec.ts
+++ test/repositories/inMemoryPlanRepository.spec.ts
@@ -1,38 +1,4 @@
-import InMemoryPlanRepository, {
-  InMemoryPlanRepository as InMemory2024PlanRepository,
-} from '@/repositories/inMemoryPlan';
-
-describe('InMemory2024PlanRepository', () => {
-  const db = {
-    '2024-11-26': {
-      date: '2024-11-26',
-      scope: '創世紀 23',
-      content: [
-        '1. 從亞伯拉罕與赫人和以弗倫的對話中你可以看見他是一位怎麼樣的人呢?為什麼?',
-        '2. 亞伯拉罕的品格有什麼值得你學習的地方?',
-      ],
-    },
-  };
-
-  const repo = new InMemory2024PlanRepository(db);
-
-  describe('findById()', () => {
-    it('returns plan object', async () => {
-      expect(await repo.findById('2024-11-26')).toMatchObject({
-        date: '2024-11-26',
-        scope: '創世紀 23',
-        content: [
-          '1. 從亞伯拉罕與赫人和以弗倫的對話中你可以看見他是一位怎麼樣的人呢?為什麼?',
-          '2. 亞伯拉罕的品格有什麼值得你學習的地方?',
-        ],
-      });
-    });
-
-    it('returns null', async () => {
-      expect(await repo.findById('2024-11-32')).toBeNull();
-    });
-  });
-});
+import InMemoryPlanRepository from '@/repositories/inMemoryPlan';
 
 describe('InMemoryPlanRepository', () => {
   const db = {
