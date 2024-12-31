aca08ba4fb121590229de797569d94fd419e71ea
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-12-31 13:39:02 +0800

    Update Plan entity to 2025 format

--- src/readingPlans/entities/plan.ts
+++ src/readingPlans/entities/plan.ts
@@ -9,3 +9,33 @@ export const OldPlanSchema = z.object({
 });
 
 export type OldPlan = z.infer<typeof OldPlanSchema>;
+
+const defaultValues = {
+  repentence:
+    '上帝啊，求你按你的慈愛恩待我！\n按你極大的憐憫除去我 ___ 的過犯！\n求你洗淨我的罪過，清除我的罪惡。求你讓我重新享受蒙你拯救的喜樂，賜我一個樂意順服你的心靈。',
+  devotional: ['1. 你覺得神透過今天的經文對你說什麼呢?', '2. 有什麼你可以做出的行動或改變呢?'],
+  prayer:
+    '”神啊！我將我的，___ ， ___ ， ___ 交給祢，我相信祢在這些事上掌權。\n『我們在天上的父：願人都尊你的名為聖。願你的國降臨；願你的旨意行在地上，如同行在天上。我們日用的飲食，今日賜給我們。免我們的債，如同我們免了人的債。不叫我們陷入試探；救我們脫離那惡者。因為國度、權柄、榮耀，全是你的，直到永遠。阿們！』”',
+};
+
+export const PlanSchema = z.object({
+  date: z.string().openapi({ example: '2025-01-01' }),
+  praise: z.object({
+    scope: z.string().openapi({ example: '歷代志上 16:34 CCB' }),
+    content: z.string().openapi({ example: '你們要稱謝耶和華, 因為祂是美善的, 祂的慈愛永遠長存!' }),
+  }),
+  repentence: z.string().default(defaultValues['repentence']).openapi({
+    example: defaultValues['repentence'],
+  }),
+  devotional: z.object({
+    scope: z.string().openapi({ example: '出埃及記 第八章' }),
+    content: z.array(z.string()).default(defaultValues['devotional']).openapi({
+      example: defaultValues['devotional'],
+    }),
+  }),
+  prayer: z.string().default(defaultValues['prayer']).openapi({
+    example: defaultValues['prayer'],
+  }),
+});
+
+export type Plan = z.infer<typeof PlanSchema>;
