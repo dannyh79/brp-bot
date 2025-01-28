514d7775d694de9d91b3b8479dbd71b1cc414a2e
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2025-01-28 17:30:44 +0800

    Extract D1 test helper function insertPlanRecord()

--- test/worker/rest.spec.ts
+++ test/worker/rest.spec.ts
@@ -1,25 +1,8 @@
-import { SELF, env } from 'cloudflare:test';
-
-const data = {
-  date: '2025-01-01',
-  praise: {
-    scope: '歷代志上 16:34 CCB',
-    content: '你們要稱謝耶和華，因為祂是美善的，祂的慈愛永遠長存！',
-  },
-  devotional: {
-    scope: '出埃及記 第八章',
-  },
-};
+import { SELF } from 'cloudflare:test';
+import { insertPlanRecord } from 'test/helpers/d1';
 
 beforeEach(async () => {
-  await env.DB.prepare(
-    `
-    INSERT INTO plans (date, praise_scope, praise_content, devotional_scope)
-    VALUES (?1, ?2, ?3, ?4)
-    `,
-  )
-    .bind(data.date, data.praise.scope, data.praise.content, data.devotional.scope)
-    .run();
+  await insertPlanRecord();
 });
 
 const stubDomain = 'https://brp-bot.pages.dev';
