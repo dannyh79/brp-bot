e31fe8f874f294137a0bcd4e7eefdc27e29fcaa8
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2025-02-01 20:42:16 +0800

    Add TODO tests to script writeToD1FromGoogleSheets

---
+++ test/scripts/writeToD1FromGoogleSheets.spec.ts
@@ -0,0 +1,14 @@
+/**
+ * FIXME:
+ * Impl when @cloudflare/vitest-pool-workers supports Vitest v3, for script impl uses Node APIs and Vitest defaults to browser APIs only.
+ * https://github.com/cloudflare/workers-sdk/pull/7923
+ * https://developers.cloudflare.com/workers/testing/vitest-integration/configuration/
+ * https://vitest.dev/guide/workspace
+ */
+describe.todo('script writeToD1FromGoogleSheet', () => {
+  it.todo('prints help message');
+  it.todo('throws when authentication fails');
+  it.todo('reads data from GoogleSheets');
+  it.todo('writes row data to D1 local database');
+  it.todo('writes row data to D1 remote database');
+});
